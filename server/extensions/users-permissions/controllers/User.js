a6f19dfff235501688a5c4c5fd96af123a0010ff
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-05-27 12:17:02 +0100

    Update server with default card

--- server/extensions/users-permissions/controllers/User.js
+++ server/extensions/users-permissions/controllers/User.js
@@ -19,77 +19,24 @@ const formatError = error => [
 ];
 
 module.exports = {
-  async updateMe(ctx) {
-    const advancedConfigs = await strapi
-      .store({
-        environment: '',
-        type: 'plugin',
-        name: 'users-permissions',
-        key: 'advanced',
-      })
-      .get();
+  async update(ctx) {
+    const { id } = ctx.params;
 
-    const { id } = ctx.state.user;
+    const { card_token } = ctx.request.body;
 
-    const { email, username, password } = ctx.request.body;
-
-    const user = await strapi.plugins['users-permissions'].services.user.fetch({
-      id,
-    });
-
-    if (_.has(ctx.request.body, 'email') && !email) {
-      return ctx.badRequest('email.notNull');
+    if (_.has(ctx.request.body, 'card_token') && !card_token) {
+      return ctx.badRequest('card_token.notNull');
     }
 
-    if (_.has(ctx.request.body, 'username') && !username) {
-      return ctx.badRequest('username.notNull');
-    }
-
-    if (_.has(ctx.request.body, 'password') && !password && user.provider === 'local') {
-      return ctx.badRequest('password.notNull');
-    }
-
-    if (_.has(ctx.request.body, 'username')) {
-      const userWithSameUsername = await strapi
-        .query('user', 'users-permissions')
-        .findOne({ username });
-
-      if (userWithSameUsername && userWithSameUsername.id != id) {
-        return ctx.badRequest(
-          null,
-          formatError({
-            id: 'Auth.form.error.username.taken',
-            message: 'username.alreadyTaken.',
-            field: ['username'],
-          })
-        );
-      }
-    }
-
-    if (_.has(ctx.request.body, 'email') && advancedConfigs.unique_email) {
-      const userWithSameEmail = await strapi.query('user', 'users-permissions').findOne({ email });
-
-      if (userWithSameEmail && userWithSameEmail.id != id) {
-        return ctx.badRequest(
-          null,
-          formatError({
-            id: 'Auth.form.error.email.taken',
-            message: 'Email already taken',
-            field: ['email'],
-          })
-        );
-      }
-    }
 
     let updateData = {
       ...ctx.request.body,
     };
 
-    if (_.has(ctx.request.body, 'password') && password === user.password) {
-      delete updateData.password;
-    }
+    console.log(updateData);
 
     const data = await strapi.plugins['users-permissions'].services.user.edit({ id }, updateData);
+    console.log(data);
 
     ctx.send(sanitizeUser(data));
   },
